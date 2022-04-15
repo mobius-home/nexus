9a43ec689e8ce10483fb6041e32e27705486481d
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-04-15 14:44:39 -0700

    Last reported time in UI

--- lib/nexus_web/live/product_live.ex
+++ lib/nexus_web/live/product_live.ex
@@ -45,12 +45,14 @@ defmodule NexusWeb.ProductLive do
         <thead>
           <tr>
             <th class="text-left border-b-2 font-medium text-sm p-4 pb-2 text-gray-600">Serial number</th>
+            <th class="text-left border-b-2 font-medium text-sm p-4 pb-2 text-gray-600">Last reported</th>
           </tr>
         </thead>
         <tbody id="devices" phx-update="prepend">
           {#for d <- @devices}
             <tr id={d.serial_number} class="even:bg-gray-100 font-light text-gray-500">
               <td class="p-4">{d.serial_number}</td>
+              <td class="p-4"></td>
             </tr>
           {/for}
         </tbody>
