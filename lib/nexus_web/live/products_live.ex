9bed86d09fc3e49633fe416053b37f62f74ba5fb
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-04-15 13:58:01 -0700

    Add some deisgn to products page

--- lib/nexus_web/live/products_live.ex
+++ lib/nexus_web/live/products_live.ex
@@ -24,6 +24,28 @@ defmodule NexusWeb.ProductsLive do
               to={Routes.live_path(@socket, NexusWeb.ProductLive, product.slug)}
               class="block"
             />
+
+            <div class="flex justify-start mt-10">
+              <div class="border-r py-2 w-1/4">
+                <p class="font-extralight mb-2">Devices</p>
+                <p>500</p>
+              </div>
+
+              <div class="border-r py-2 pl-5 w-1/4">
+                <p class="font-extralight mb-2">Metrics</p>
+                <p>20</p>
+              </div>
+
+              <div class="border-r py-2 pl-5 w-1/4">
+                <p class="font-extralight mb-2">Latest Firmware</p>
+                <p>1.5.5</p>
+              </div>
+
+              <div class="py-2 pl-5 w-1/4">
+                <p class="font-extralight mb-2">Other Info</p>
+                <p>1234</p>
+              </div>
+            </div>
           </div>
         {/for}
       </div>
