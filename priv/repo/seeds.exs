b6e7e6676f5fd17b2b692c3efe58323d278f8897
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-06-07 11:30:35 -0700

    Clean up seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,34 +12,6 @@
 
 alias Nexus.Accounts
 
-# product =
-#   case Products.get_product_by_slug("example-product") do
-#     nil ->
-#       {:ok, product} = Products.create_product("Example Product")
-#       product
-
-#     product ->
-#       product
-#   end
-
-# case Products.get_device_for_product_by_device_slug(product, "ex123board") do
-#   nil ->
-#     {:ok, device} = Products.create_device_for_product(product, "EX123BOARD")
-#     device
-
-#   device ->
-#     device
-# end
-
-# case Products.get_metric_for_product_by_slug(product, "vm-memory-total-last-value") do
-#   nil ->
-#     {:ok, metric} = Products.create_metric_for_product(product, "vm.memory.total", "last_value")
-#     metric
-
-#   metric ->
-#     metric
-# end
-
 user_roles = ["admin", "product_maintainer", "user"]
 
 for role <- user_roles do
